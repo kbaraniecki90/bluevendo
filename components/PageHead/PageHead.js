75bd367cfed02108dbe901c7643b6f74f34bb611
kbaraniecki90 <kbaraniecki@wp.pl>
2023-06-15 23:07:41 +0200

    Added PageHead component

---
+++ components/PageHead/PageHead.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "next/head";
+
+const PageHead = () => {
+  return (
+    <Head>
+      <title>BlueVendo - Krzysztof Baraniecki</title>
+    </Head>
+  );
+};
+
+export default PageHead;
