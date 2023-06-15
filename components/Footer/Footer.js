6ac3af4afc1b46fa23ce79436abe13670c0309e6
kbaraniecki90 <kbaraniecki@wp.pl>
2023-06-15 23:06:27 +0200

    Added Footer component

---
+++ components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Container from "../Container/Container";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <p>2023 BlueVendo - Krzysztof Baraniecki</p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
