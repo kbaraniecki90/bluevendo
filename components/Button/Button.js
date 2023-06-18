59c34730acb0878ea026946fcae05f36ecd4be43
kbaraniecki90 <kbaraniecki@wp.pl>
2023-06-18 22:14:58 +0200

    Add type annotations for Button component

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,22 +1,52 @@
 import React from "react";
 import styled from "styled-components";
 
+const BaseButtonStyles = `
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  text-decoration: none;
+  transition: background-color .3s, color .3s ;
+`;
+
 const ButtonWrapper = styled.button`
+  ${BaseButtonStyles}
+
   background-color: #c7e7f5;
-  color: #000000;
   padding: 15px 37px;
+  color: #000000;
   border: none;
   border-radius: 6px;
   font-size: 16px;
-  cursor: pointer;
 
   &:hover {
     background-color: #aac8d4;
+    color: #ffffff;
   }
 `;
 
-const Button = ({ children, onClick }) => {
-  return <ButtonWrapper onClick={onClick}>{children}</ButtonWrapper>;
+const LinkWrapper = styled.a`
+  ${BaseButtonStyles}
+
+  padding: 3.5px 18px;
+  color: #3068de;
+  font-weight: 600;
+
+  &:hover {
+    color: #2c5dc5;
+  }
+`;
+
+const Button = ({ children, onClick, icon, type }) => {
+  const WrapperComponent = type === "link" ? LinkWrapper : ButtonWrapper;
+
+  return (
+    <WrapperComponent onClick={onClick}>
+      {children} {icon && <img src={icon} />}
+    </WrapperComponent>
+  );
 };
 
 export default Button;
